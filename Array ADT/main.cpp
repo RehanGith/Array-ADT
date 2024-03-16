682f212f186aa17b8379a7e1d97de01424f74729
RehanGith <chrehan1007@gmail.com>
2024-03-16 05:18:06 +0500

    add and display M_function

--- Array ADT/main.cpp
+++ Array ADT/main.cpp
@@ -17,10 +17,37 @@ public:
 Array::Array(int n) : size(n), length(0) {
 	arr = new int[size];
 }
+void Array::add() {
+	if (length - 1 == size) {
+		cout << "Array is full" << endl;
+		return;
+	}
+	while (true) {
+		cout << "How many number you wants to add: ";
+		cin >> length;
+		if (length <= size) {
+			break;
+		}
+		cout << "Please provide length within the bound" << endl;
+	}
+	cout << "Enter data: ";
+	for (int i = 0; i < length; i++) {
+		cin >> arr[i];
+	}
 
+}
+void Array::display() {
+	cout << "Data: ";
+	for (int i = 0; i < length; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
 Array::~Array() {
 	delete[] arr;
 }
 int main() {
-	
+	Array a1(15);
+	a1.add();
+	a1.display();
 }
